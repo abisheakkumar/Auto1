395eb8dbf7a3f3ebb5cd2c551c5790892b515d0b
abisheakkumar <abisheakkumar29@gmail.com>
2024-01-26 08:32:58 +0530

    Newly commiteed

--- src/test/java/practise/First.java
+++ src/test/java/practise/First.java
@@ -1,7 +1,11 @@
 package practise;
 
+import org.testng.annotations.Test;
+
 public class First {
-	public static void main(String[] args) {
+	
+	@Test
+	public void first() {
 		System.out.println("Hi");
 	}
 }
